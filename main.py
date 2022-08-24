5b80d477b2de13bef28536183a7e587b964e0d1e
rdavcardoso <rogerio.davcardoso@gmail.com>
2022-08-24 20:04:11 -0300

    Corrigindo os erros com a senha, criar conta e login redirecionando corretamente quando submetidas com sucesso.

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from forms import FormLogin, FormCriarConta
 
 app = Flask(__name__)
 
-users_list = ['Rogerio', 'Roze', 'Rafael', 'Elayne', 'Jefferson']
+users_list = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
-app.config['SECRET_KEY'] = '60fad3b42e2ac1ba2f205aed713c10e0'
+app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
 
 
 @app.route('/')
@@ -23,18 +23,16 @@ def users():
     return render_template('users.html', users_list=users_list)
 
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no email: {form_login.email.data}', 'alert-success')
+        flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
-        flash(f'Conta criada para o email: {form_criarconta.email.data}', 'alert-success')
+        flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
-
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
 
 
