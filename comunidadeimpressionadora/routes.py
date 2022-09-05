cc56817c88705ca572c0be27f403e54a4d7a064c
rdavcardoso <rogerio.davcardoso@gmail.com>
2022-09-05 19:01:58 -0300

    Criado a lógica para salvar o preenchimento do forms no comunidade.db e importado e implementado criptografia para a senha, testes no console funcionaram perfeitamente.

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
-from comunidadeimpressionadora import app
+from comunidadeimpressionadora import app, database, bcrypt
 from comunidadeimpressionadora.forms import FormLogin, FormCriarConta
+from comunidadeimpressionadora.models import Usuario
 
 
 users_list = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
@@ -28,6 +29,11 @@ def login():
         flash(f'Login feito com sucesso no e-mail: {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data,
+                          email=form_criarconta.email.data, senha=senha_cript)
+        database.session.add(usuario)
+        database.session.commit()
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
